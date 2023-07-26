81dd47869781ac86372e3ec599d4cd78abf17108
Corina <corina@danitt.com>
2023-07-26 23:00:49 +1000

    fix merge conflict issue

--- testing/student_update.test.js
+++ testing/student_update.test.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const { app } = require('../src/server');
-const Student = require('../src/models/students'); 
-<<<<<<< HEAD
+const Student = require('../src/models/students');
 const { createStudentToken } = require('../src/services/students_auth_service');
 const validateStudentRequest = require('../src/middlewares/students_auth_middleware')
-=======
-const { createStudentToken } = require('../src/services/users_auth_service');
-const validateStudentRequest = require('../src/middlewares/auth_middleware')
->>>>>>> main
 
 require("dotenv").config();
 
@@ -23,11 +18,6 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-<<<<<<< HEAD
-// Add this before all the test cases
-let validToken; // Declare a variable to store the JWT
-
-=======
 beforeAll(() => {
   // Apply the middleware to the app
   app.use(validateStudentRequest);
@@ -36,7 +26,6 @@ beforeAll(() => {
 // Add this before all the test cases
 let validToken; // Declare a variable to store the JWT
 
->>>>>>> main
 beforeAll(async () => {
   // Create a new student and save it to the database (use your Student model or database mock)
   const newStudent = new Student({
@@ -46,7 +35,6 @@ beforeAll(async () => {
     email: 'john.doe@example.com',
     password: 'hashed_password',
     lessons: ["64bafbd999bdd75bc6046b6d"]
-<<<<<<< HEAD
   });
   const savedStudent = await newStudent.save();
 
@@ -95,60 +83,5 @@ describe('updateStudent', () => {
 
     // Check the response status code
     expect(response.status).toBe(404);
-=======
->>>>>>> main
-  });
-  const savedStudent = await newStudent.save();
-
-  // Create a valid JWT for the student and store it in the variable
-  validToken = createStudentToken(savedStudent.student_id, savedStudent.email);
-});
-
-<<<<<<< HEAD
-// Apply the validateStudentRequest middleware after the route definitions
-app.use(validateStudentRequest);
-=======
-describe('updateStudent', () => {
-  it('should update a student profile with a valid JWT', async () => {
-    // Define the updated data for the student profile
-    const updatedData = {
-      _id: '64baa41f334238e44139660a',
-      firstName: 'Johnathan',
-      lastName: 'Dow',
-      email: 'john.doe@example.com',
-      password: 'hashed_password',
-      lessons: ["64bafbd999bdd75bc6046b6d"]
-    };
-
-    // Send the request to update the student profile with the valid JWT
-    const response = await request(app)
-      .put("/profile/student")
-      .set('Authorization', `Bearer ${validToken}`)
-      .send(updatedData);
-
-    // Check the response status code and content
-    expect(response.status).toBe(201);
-    expect(response.body).toMatchObject(updatedData);
-  });
-
-  it('should return 404 if student not found', async () => {
-    // Create a valid but non-existent student ID
-    const nonExistentStudentID = 'non-existent-student-id';
-    const validToken = createStudentToken(nonExistentStudentID, 'someemail@example.com');
-
-    // Define the updated data for the student profile
-    const updatedData = {
-      firstName: 'Johnathan',
-    };
-
-    // Send the request to update the student profile with the valid JWT
-    const response = await request(app)
-      .put("/profile/student")
-      .set('Authorization', `Bearer ${validToken}`)
-      .send(updatedData);
-
-    // Check the response status code
-    expect(response.status).toBe(404);
   });
 });
->>>>>>> main
