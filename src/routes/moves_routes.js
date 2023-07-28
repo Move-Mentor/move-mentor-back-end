9e8fd2d306ba75291d6bb6875e84f215fe8e27fb
KarlaB3 <k.bucoy@gmail.com>
2023-07-29 05:06:33 +1000

    test: moves unit testing

--- src/routes/moves_routes.js
+++ src/routes/moves_routes.js
@@ -17,22 +17,4 @@ movesRouter.get("/categories/:category", getCategoryMoves)
 // Retrieve a specific move
 movesRouter.get("/:id", getSpecificMove)
 
-
-// Retrieve and display a specific move assigned to a category
-movesRouter.get("/categories/:categoryId/:moveId", (request, response) => {
-   // Must include an authorisation check to activate and display the 'add to class' button only for teachers
-  response.json(
-    {message: "this is the details of a specific move assigned to a category"}
-  )
-})
-
-
-// Add a specific move from a class
-movesRouter.put("/categories/:categoryId/:moveId", (request, response) => {
-  // Must include an authorisation check to activate and display this method only for teachers
-  response.json(
-    {message: "this adds a move to a class"}
-  )
-}) 
-
 module.exports = movesRouter
--- testing/moves.test.js
+++ testing/moves.test.js
@@ -1,87 +1,59 @@
-// const mongoose = require('mongoose');
-// const request = require('supertest');
-// const { databaseConnector } = require("../src/database");
-// const { app } = require('../src/server');
+const mongoose = require('mongoose');
+const request = require('supertest');
+const { databaseConnector } = require("../src/database");
+const Move = require('../src/models/moves')
+const { app } = require('../src/server');
 
-// // Test student sign up 
-// describe("Moves...", () => {
+// Test category data retrievals
+describe("...categories functionality...", () => {
+  // Connect to the test database
+  beforeEach(async () => {
+  await databaseConnector();
+  });
 
-//   // Connect to the test database
-//   beforeAll(async () => {
-//     await databaseConnector();
-//   });
+  // Drop the database and close the connection after all tests
+  afterEach(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  });
 
-//   // Drop the database and close the connection after all tests
-//   afterAll(async () => {
-//     await mongoose.connection.dropDatabase();
-//     await mongoose.connection.close();
-//   });
+  describe ("...getting a list of all categories...", () => {
+    it("...responds with status 200", async () => {
+      const moveCategories = [
+        {moveName:"Allegra", moveCategory: "inverts"}, 
+        {moveName: "Sandra", moveCategory: "spins"}, 
+        {moveName: "Stag Pose", moveCategory: "aerials"}
+      ]
+      await Move.insertMany(moveCategories);
+      
+      const response = await request(app).get("/moves/categories")
+      expect(response.statusCode).toBe(200);
+    })
+  })
+    
+    it("...should return with a 404 error if no categories are found", async () => {
+      const response = await request(app).get("/moves/categories")
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toEqual({Error: "No categories found."})
+    })
 
-//   describe("...categories functionality...", () => {
+  describe ("...getting a specific move...", () => {
+    it("...responds with status 200", async () => {
+      const specificMove = new Move({
+        moveName: "Brass Monkey - Hold",
+        moveCategory: "inverts",
+        moveAlternativeName: "Funky Monkey",
+      })
+      
+      await Move.create(specificMove);
+      
+      const response = await request(app).get(`/moves/${specificMove._id}`);
+      expect(response.statusCode).toBe(200);
+    })
+  })
 
-//     it("...responds with status 200 on successful retrieval of categories", async () => {
-//       const response = await request(app).get("/moves/categories")
-//       expect(response.statusCode).toBe(200);
-//     })
+        
+  })
+  
+    // Additional testing required for getCategoryMoves
 
-//     it("...responds with a JSON object on successful retrieval of categories", async () => {
-//       const response = await request(app).get("/moves/categories")
-//       const responseBodyDataType = typeof (response.body);
-//       expect(responseBodyDataType).toBe("object");
-//     })
-
-//     it("...responds with status 404 if no categories are found", async () => {
-//       const response = await request(app).get("/moves/categories")
-//       expect(response.statusCode).toBe(404);
-//     })
-
-//   });
-
-
-//   describe("...can sign up...", () => {
-//     it("...with a first name, last name, email, password and lesson", async () => {
-//       const response = await request(app)
-//         .post("/users/signup/student")
-//         .send({
-//           firstName: "Jen",
-//           lastName: "Student",
-//           email: "studentjen@email.com",
-//           password: "somepassword",
-//           lessons: ["64bd5c8bb3a65f2dbf60295f"]
-//         })
-//       expect(response.statusCode).toBe(201);
-//       expect(response.body.email).toBe("studentjen@email.com")
-//     })
-//   })
-
-//   describe("...can not sign up...", () => {
-//     it("...if an email is already registered", async () => {
-//       const response = await request(app)
-//         .post("/users/signup/student")
-//         .send({
-//           firstName: "Jen",
-//           lastName: "Student",
-//           email: "studentjen@email.com",
-//           password: "somepassword",
-//           lessons: ["64bd5c8bb3a65f2dbf60295f"]
-//         })
-//       expect(response.statusCode).toBe(409);
-//       expect(response.body).toEqual({ Error: "Email is already registered." })
-//     })
-//   })
-
-//   describe("...can not sign up...", () => {
-//     it("...when a signup field is not entered", async () => {
-//       const response = await request(app)
-//         .post("/users/signup/student")
-//         .send({
-//           firstName: "Sam",
-//           lastName: "",
-//           email: "studentsam@email.com",
-//           password: "somepassword",
-//           lessons: ["64bd5c8bb3a65f2dbf60295f"]
-//         })
-//       expect(response.statusCode).toBe(400);
-//     })
-//   })
-// }) 
