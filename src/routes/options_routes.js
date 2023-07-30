c6abd83dfb221077242a05fa309805114c2a0499
KarlaB3 <k.bucoy@gmail.com>
2023-07-31 07:15:35 +1000

    refactor: updated options route to just one route for both students and teachers

--- src/routes/options_routes.js
+++ src/routes/options_routes.js
@@ -4,18 +4,10 @@ const express = require('express')
 // Create the user router instance
 const optionsRouter = express.Router();
 
-// View student options 
-optionsRouter.get("/student", (request, response) => {
+// View options cards 
+optionsRouter.get("/", (request, response) => {
   response.json(
-    {Message: "This is to view the student option cards: Class Moves, Profile, All Moves"}
-  )
-}) 
-
-// View options ccards
-optionsRouter.get("/teacher", (request, response) => {
-  // Must include authorisation check because the cards displayed to students and teachers differ
-  response.json(
-    {Message: "This is to view the teacher option cards: Lessons, Profile, All Moves"}
+    {Message: "This is to view the lessons, profile and moves options cards."}
   )
 }) 
 
